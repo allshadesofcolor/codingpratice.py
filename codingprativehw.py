ac776030477d034cfd693d430ace8f57b1ddbb08
allshadesofcolor <163556067+allshadesofcolor@users.noreply.github.com>
2024-03-23 12:46:03 -0500

    commit 4

--- codingprativehw.py
+++ codingprativehw.py
@@ -14,4 +14,24 @@ if mood == ("Sad"):
 Pi_value = 3.14
 user_age = 25
 user_location = ("New York")
-Max_limit = > 1000 
+ 
+
+
+mystery_variable ="Sherlock"
+clue_list = ["candlestick", "footprint", "handkerchief"]
+alleged_title = "I am duke"
+
+print("type of mystery varable:", type(mystery_variable))
+
+variable_a = "Hello, world"
+print("type of variable a:", type(variable_a))
+
+variable_b = 23
+print("type of variable b:", type(variable_b))
+
+variable_c = 3.14
+print("type of varable c", type (variable_c))
+
+variable_d = True
+print("type of variable d"), type (variable_d)
+      
