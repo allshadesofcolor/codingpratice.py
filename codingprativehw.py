2e6625c57749474fc82687c47b79d716c52e6fbc
allshadesofcolor <163556067+allshadesofcolor@users.noreply.github.com>
2024-03-23 11:44:41 -0500

    commit 2

--- codingprativehw.py
+++ codingprativehw.py
@@ -3,3 +3,11 @@ if weather == ("sunny"):
     print("wear sunglasses!")
 else:
     print("take an umbrella!")
+
+
+mood = ("Happy")
+if mood == ("Happy"):
+    print("Thats's great to hear!")
+if mood == ("Sad"):
+    print("I hope your day gets better!")
+
